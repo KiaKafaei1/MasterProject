9d3e6e2617a757d91ad063ab1b4a064e8f5bbb24
Kia <kia0896@yahoo.dk>
2020-08-24 15:20:25 +0200

    Writes data to a csv file

--- src/read_write.py
+++ src/read_write.py
@@ -1,22 +1,41 @@
+#Using the ElemenTree library to handle XML data
 import xml.etree.ElementTree as ET
-
+import csv
 
 path ='WayFinding/127/641299/'
 tree = ET.parse(path+'rooms.xml')
 root = tree.getroot()
-print(root.tag)
 rooms_cord = []
+#Getting the coordinates by looping through each room
 for child in root:
     for grandchild in child:
         if(grandchild.tag=='Triangles'): 
-            #print(grandchild.attrib)
             rooms_cord.append(grandchild.attrib)
-print(rooms_cord)
-coord =[]
-for dic in rooms_cord:
-    coord.append(dic['Coords'])
-print(coord)
 
+
+
+
+#Writing to CSV file. Used this for help: https://www.geeksforgeeks.org/working-csv-files-python/
+fields = ['Coords']
+filename = "room_coordinates.csv"
+
+with open(filename,'w') as csvfile:
+    writer = csv.DictWriter(csvfile,fieldnames = fields)
+    writer.writeheader()
+    writer.writerows(rooms_cord)
+
+
+
+
+
+
+
+
+#coord =[]
+#for dic in rooms_cord:
+#    coord.append(dic['Coords'])
+#print(coord)
+#
 
 
 

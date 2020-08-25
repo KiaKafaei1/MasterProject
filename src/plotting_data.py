fa6bf158f53ee4d816861649dfc995d143d822f9
Kia <kia0896@yahoo.dk>
2020-08-25 16:05:11 +0200

    In the middle of giving hashes to lines

--- src/plotting_data.py
+++ src/plotting_data.py
@@ -39,64 +39,47 @@ for ele in array_new2:
 
 
 
-#Putting 3 following coordinates together to make up triangles
+##Putting 3 following coordinates together to make up triangles
 array_new4= []
+#for ele in array_new3:
+#    for i in range(0,len(ele)-1,3):
+#         ele_temp.append([ele[i],ele[i+1],ele[i+2]])
+#    array_new4.append(ele_temp)
+#    ele_temp = []
+#
+#
+##plotting triangles
+#tempx=[]
+#tempy=[]
+#temp = array_new4[3]
+#for ele in array_new4:
+#    for tri in ele:
+#        for pair in tri:
+#            for i in range(0,len(pair)):
+#                if i ==0:
+#                    tempx.append(pair[i])
+#                else:
+#                    tempy.append(pair[i])
+#        tempx.append(tempx[0])
+#        tempy.append(tempy[0])
+#        plt.plot(tempx,tempy)
+#        tempx=[]
+#        tempy=[]
+#    
+#plt.show()
+
+
+# From list to tuple since we have to use tuple for hashing
+tup = ()
 for ele in array_new3:
-    for i in range(0,len(ele)-1,3):
-         ele_temp.append([ele[i],ele[i+1],ele[i+2]])
-    array_new4.append(ele_temp)
-    ele_temp = []
+    tup = tuple(tuple(x) for x in ele)
+    array_new4.append(tup)
+    tup = ()
+print(array_new4[3])
 
 
-#plotting triangles
-tempx=[]
-tempy=[]
-temp = array_new4[3]
-for ele in array_new4:
-    for tri in ele:
-        for pair in tri:
-            print(pair)
-            for i in range(0,len(pair)):
-                print(i)
-                print(len(pair))
-                if i ==0:
-                    tempx.append(pair[i])
-                else:
-                    tempy.append(pair[i])
-        tempx.append(tempx[0])
-        tempy.append(tempy[0])
-        plt.plot(tempx,tempy)
-        tempx=[]
-        tempy=[]
-    
+
+#### TESTING HASHING
+plt.plot([0,1],[1,0])
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 

84a64b322efe8e51d7e324ce6e516243cec7c028
Kia <kia0896@yahoo.dk>
2020-08-26 09:05:53 +0200

    Did some house cleaning on the variable names, more specifically the array variable

--- src/plotting_data.py
+++ src/plotting_data.py
@@ -37,23 +37,24 @@ for ele in array_new2:
     array_new3.append(ele_temp)
     ele_temp = []
 
-
+# The array after processing the data
+array = array_new3
 
 #Putting 3 following coordinates together to make up triangles
-array_new4= []
-for ele in array_new3:
+array_tri= []
+for ele in array:
     for i in range(0,len(ele)-1,3):
          ele_temp.append([ele[i],ele[i+1],ele[i+2]])
         # hash(frozenset(ele[i+2]))# identifying the hypotenuse coordinate
          
-    array_new4.append(ele_temp)
+    array_tri.append(ele_temp)
     ele_temp = []
 
 #
 ##plotting triangles
 tempx=[]
 tempy=[]
-for ele in array_new4:
+for ele in array_tri:
     for tri in ele:
         for pair in tri:
             for i in range(0,len(pair)):
@@ -71,29 +72,27 @@ plt.show()
 
 
 # From list to tuple since we have to use tuple for hashing
-#tup = ()
-#for ele in array_new3:
-#    tup = tuple(tuple(x) for x in ele)
-#    array_new4.append(tup)
-#    tup = ()
-#print(array_new4[3])
-#
+array_tup =[]
+tup = ()
+for ele in array:
+    tup = tuple(tuple(x) for x in ele)
+    array_tup.append(tup)
+    tup = ()
+print(array_tup[3])
+
 
 
 #Making Frozenset
-
-#fro1=None 
-#fro2 =None 
-#
-#for ele in array_new3:
-#    for i in range(0,len(ele)):
-#        if i%2==0:
-#            fro1 = frozenset(ele[i])
-#        else:
-#            fro2 = frozenset(ele[i])
-#        if fro1==None or fro2==None:
-#            continue;
-#        #print(hash(fro1,fro2))
-            
-        
-#print(hash(frozenset(frozenset([1,2]),frozenset([3,1]))))
+fro1=None 
+fro2 =None 
+temp1 = []
+temp2 = []
+for ele in array_new3:
+    for i in range(0,len(ele)):
+        if i%2==0:
+            temp1 = ele[i]
+        else:
+            temp2 = ele[i]
+        if temp1==None or temp2==None:
+            continue;
+       # print(hash(frozenset([temp1,temp2])))

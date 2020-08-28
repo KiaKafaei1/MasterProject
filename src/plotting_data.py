d4e48e96304e74ae2a31c60c83197835723ef518
Kia <kia0896@yahoo.dk>
2020-08-28 20:13:10 +0200

    This code includes hashing and plotting, it is the intermediary script before implementing dictionaries with hashes as key values

--- src/plotting_data.py
+++ src/plotting_data.py
@@ -10,7 +10,6 @@ array = df.to_numpy()
 array = np.delete(array,0)#deleting the header
 
 # The coordinates are in string form and will have to be changed to float
-
 # Splitting the strings in the array with spaces
 array_new = []
 for ele in array:
@@ -40,59 +39,143 @@ for ele in array_new2:
 # The array after processing the data
 array = array_new3
 
+## From list to tuple since we have to use tuple for hashing
+#array_tup =[]
+#tup = ()
+#for ele in array:
+#    tup = tuple(tuple(x) for x in ele)
+#    array_tup.append(tup)
+#    tup = ()
+#array = array_tup
+
+
 #Putting 3 following coordinates together to make up triangles
 array_tri= []
 for ele in array:
     for i in range(0,len(ele)-1,3):
          ele_temp.append([ele[i],ele[i+1],ele[i+2]])
-        # hash(frozenset(ele[i+2]))# identifying the hypotenuse coordinate
-         
     array_tri.append(ele_temp)
     ele_temp = []
 
-#
-##plotting triangles
+
+#plotting triangles
+hash_values = []
 tempx=[]
 tempy=[]
-for ele in array_tri:
-    for tri in ele:
+i=0
+fig, ax = plt.subplots()
+
+for room in array_tri:
+    for tri in room:
         for pair in tri:
-            for i in range(0,len(pair)):
-                if i ==0:
-                    tempx.append(pair[i])
-                else:
-                    tempy.append(pair[i])
-        tempx.append(tempx[0])
-        tempy.append(tempy[0])
-        plt.plot(tempx,tempy)
-        tempx=[]
-        tempy=[]
-    
+            tempx.append(pair[0])
+            tempy.append(pair[1])
+        #tempx.append(tempx[0])
+        #tempy.append(tempy[0])
+        #print(pair) 
+        #hashing each line in triangle
+        hash1= hash(((tempx[0],tempy[0]),(tempx[1],tempy[1])))
+        hash12= hash(((tempx[1],tempy[1]),(tempx[0],tempy[0])))
+        hash2= hash(((tempx[0],tempy[0]),(tempx[2],tempy[2])))
+        hash22= hash(((tempx[2],tempy[2]),(tempx[0],tempy[0])))
+        hash3= hash(((tempx[1],tempy[1]),(tempx[2],tempy[2])))
+        hash32= hash(((tempx[2],tempy[2]),(tempx[1],tempy[1])))
+        line1, = ax.plot([tempx[0],tempx[1]],[tempy[0],tempy[1]])
+        line2, = ax.plot([tempx[0],tempx[2]],[tempy[0],tempy[2]])
+        line3, = ax.plot([tempx[1],tempx[2]],[tempy[1],tempy[2]])
+        
+
+
+        #ax.lines.remove(line1)
+        #l1 = line1.pop(0)
+        #l1.remove()
+        #del l1
+        #line2.pop(0).remove()
+        #line3.pop(0).remove()
+        #line2.remove()
+        #line2.remove()
+        #line2.remove()
+        #line3.remove()
+        print(len(ax.lines))
+        if (hash1 in hash_values or hash12 in hash_values):
+            i+=1
+            #print(i)
+            #del ax.lines[0]
+            ax.lines.remove(line1)
+            #l1 = line1.pop(0)
+            #l1.remove()
+            #del l1
+        if (hash2 in hash_values or hash22 in hash_values):
+            k=3
+            ax.lines.remove(line2)
+            #l2 = line2.pop(0)
+            #l2.remove()              
+            #del l2                               
+        if (hash3 in hash_values or hash32 in hash_values):
+            k=3
+            ax.lines.remove(line3)
+            #l3 = line3.pop(0)
+            #l3.remove()
+            #del l3
+
+
+
+        hash_values.append(hash1) 
+        hash_values.append(hash12)
+        hash_values.append(hash2) 
+        hash_values.append(hash22) 
+        hash_values.append(hash3) 
+        hash_values.append(hash32) 
+        tempx = []
+        tempy = []
+    #plt.show()
+print(len(ax.lines))
 plt.show()
 
-
-# From list to tuple since we have to use tuple for hashing
-array_tup =[]
-tup = ()
-for ele in array:
-    tup = tuple(tuple(x) for x in ele)
-    array_tup.append(tup)
-    tup = ()
-print(array_tup[3])
+seen = []
+for number in hash_values:
+    if number in seen:
+        k=3
+        #print( "Number repeated!")
+    else:
+        seen.append(number)
 
 
 
-#Making Frozenset
-fro1=None 
-fro2 =None 
-temp1 = []
-temp2 = []
-for ele in array_new3:
-    for i in range(0,len(ele)):
-        if i%2==0:
-            temp1 = ele[i]
-        else:
-            temp2 = ele[i]
-        if temp1==None or temp2==None:
-            continue;
-       # print(hash(frozenset([temp1,temp2])))
+
+#print(hash(((1,2),(3,1))))
+#print(hash(((3,1),(1,2))))
+l = [1,2,3,4,4,3,1,2,4,4]
+l2 =[]
+#seen = set()
+#for i in range(0,len(l)-1):
+#    x=l[i]
+#    z=l[i+1]
+#    
+#    if(l
+##for x,z in zip(l[0::2],l[1::2]):
+#    if x in l2 and z in l2:
+#      print(x)
+#      print(z)
+#      continue
+# l2.append(x)
+# l2.append(z)
+#  #seen.add(x)
+#  #seen.add(z)
+
+#print(hash(((3,3),(4,3))))
+#print(hash(((3,3),(4,3))))
+
+
+
+       # hash_values.append(hash1) 
+       # hash_values.append(hash2) 
+       # hash_values.append(hash3) 
+
+        #plt.plot(tempx,tempy)
+       # line1= plt.plot([tempx[0],tempx[1]],[tempy[0],tempy[1]])
+       # line2= plt.plot([tempx[0],tempx[2]],[tempy[0],tempy[2]])
+       # line3= plt.plot([tempx[1],tempx[2]],[tempy[1],tempy[2]])
+
+
+

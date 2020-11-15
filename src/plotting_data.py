fbdcc93c4eb13e1f8688534e6fb1ae8e7ccb4dac
Kia Kafaei <s153811@student.dtu.dk>
2020-11-15 22:25:10 +0100

    Got the tsp solution to work on the big building, also added corners

--- src/plotting_data.py
+++ src/plotting_data.py
@@ -12,11 +12,13 @@ from scipy.sparse.csgraph import minimum_spanning_tree
 import networkx as nx
 import dwave_networkx as dnx
 from scipy.spatial import distance
-
+import random
 ################# Defining functions and classes ###################
 
-# We hash 6 lines because the lines may be in reverse coordinate order for different triangles, meaning that a line from x1,y1 to x2,y2 in one triangle is the line x2,y2 to x1,y1 in another triangle.
+#1
 def dict_hashing(Dic,tempx,tempy):
+    '''We hash 6 lines because the lines may be in reverse coordinate order for different triangles, meaning  that a line from x1,y1 to x2,y2 in one triangle is the line x2,y2 to x1,y1 in another triangle.'''
+
     h1=hash(((tempx[0],tempy[0]),(tempx[1],tempy[1])))
     h2=hash(((tempx[1],tempy[1]),(tempx[0],tempy[0])))
     h3=hash(((tempx[0],tempy[0]),(tempx[2],tempy[2])))
@@ -32,21 +34,33 @@ def dict_hashing(Dic,tempx,tempy):
     Dic[h6].append(((tempx[2],tempy[2]),(tempx[1],tempy[1]))) 
     return Dic
 
-#Dertimining line intersection 
-# Code taken from https://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
-class Point:
-	def __init__(self,x,y):
-		self.x = x
-		self.y = y
-# Checking if 3 points are in counterclockwise order
-def ccw(A,B,C):
-	return (C.y-A.y)*(B.x-A.x) > (B.y-A.y)*(C.x-A.x)
-# Finding if 4 points are intersecting. Se above link for explanation.
-def intersect(A,B,C,D):
-	return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
 
-# Function for plotting points
+#2
+class Point:
+    ''' A class used to save node/point objects'''
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+#3
+def ccw(A,B,C):
+    ''' 
+Checking if 3 points are in counterclockwise order
+Dertimining line intersection 
+Code taken from https://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
+'''
+    return (C.y-A.y)*(B.x-A.x) > (B.y-A.y)*(C.x-A.x)
+
+def intersect(A,B,C,D):
+    '''
+Finding if 4 points are intersecting. Se above link for explanation.
+'''
+    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+
+#4
 def plot_point(point,door=True,starting_node=False):
+    ''' 
+Function for plotting points
+'''
     if door:
         plt.plot(point.x,point.y,marker='o',color='black',markerfacecolor='black')
     elif starting_node:
@@ -55,34 +69,38 @@ def plot_point(point,door=True,starting_node=False):
         plt.plot(point.x,point.y,marker='o',color='red', markerfacecolor='red')
 
 
-# Function for plotting path 
+#5
 def plot_path(p1,q1,Dic_lines):
+    '''
+Function for plotting path.
+The function returns true if there is a traversable connection between 2 points else returns False.
+'''
     epsilon = 2
-    # Function that returns true if there is a traversable connection between 2 points
-    # else returns False
 
     # If this is true it means that it is 2 doors right next to eachother and the path should therefore be traversable
     if (math.sqrt((p1.x-q1.x)**2+(p1.y-q1.y)**2)<epsilon):
         return True
-
-    for line in Dic.values():
+    #print(Dic_lines.values())
+    #print("size of dict",len(Dic_lines))
+    for line in Dic_lines.values():
+        #print(line)
         p2 = Point(line[0][0][0],line[0][0][1])
         q2 = Point(line[0][1][0],line[0][1][1])
         
         val= intersect(p1,q1,p2,q2)
         if val:
             #They do intersect
-            break
-    if not val:
-        return True
-    return False
+            return False
+        if not val:
+            continue
+    return True
 
 
 ##################### Plotting Rooms ###########################
+#6
 #Importing values and changing from df to numpy array
 df = pd.read_csv('room_coordinates.csv',sep=',', header=None)
 room_127 = False
-#room_big = False 
 array = df.to_numpy()
 # Deleting the header
 array = np.delete(array,0)
@@ -90,13 +108,13 @@ array = np.delete(array,0)
 array = cop.cor_processing(array)
 array_tri = cop.tri_processing(array)
 
-
+#7
 #plotting triangles
 tempx=[]
 tempy=[]
 Dic =collections.defaultdict(list) 
 fig, ax = plt.subplots()
-
+Dic_all = collections.defaultdict(list)
 i=0
 for room in array_tri:
     #Not including every room to simplify the drawing
@@ -120,22 +138,21 @@ for room in array_tri:
     for x in list(Dic):
         if len(Dic[x])>1 or distance.euclidean(Dic[x][0][0],Dic[x][0][1])<0.1:
             Dic.pop(x)            
-        
+#8        
     # Manually removing lines    
     for x in list(Dic):
         coord1 = Dic[x][0][0]
         coord2 = Dic[x][0][1]
-        if (123.8<coord1[0]<123.85 and 63.6<coord1[1]<63.65) or (123.8<coord2[0]<123.85 and 63.6<coord2[1]<63.65):
-            Dic.pop(x)
+            #Dic.pop(x)
         if (123.82<coord1[0]<123.86 and 95.6<coord1[1]<95.75) or (123.82<coord2[0]<123.86 and 95.6<coord2[1]<95.75): 
-            Dic.pop(x)
-        
+            Dic.pop(x) 
         if (117<coord1[0]<118 and 95<coord1[1]<95.5) or (117<coord2[0]<118 and 95<coord2[1]<95.5): 
             Dic.pop(x)
         if (114<coord1[0]<115 and 98.5<coord1[1]<99) or (114<coord2[0]<115 and 98.5<coord2[1]<99):
             Dic.pop(x)
-    
-
+        if (123.75<coord1[0]<123.95 and 63.6<coord1[1]<63.67) or (123.75<coord2[0]<123.95 and 63.6<coord2[1]<63.67): 
+            Dic.pop(x)     
+#9
     # All lines are plotted twice due to the way we are hashing, it is more appropriate to remove replicate values that are in more than 1 hash.
     # Remove replicate lines  
     #print("length of dic before",len(Dic.keys()))
@@ -156,16 +173,16 @@ for room in array_tri:
     #print("length of dic after", len(Dic.keys()))
 
 
-
-    
-
+    Dic_all.update(Dic)
+    #print(Dic)
     # Plotting the lines
     for line in Dic.values():
         ax.plot([line[0][0][0], line[0][1][0]],[line[0][0][1], line[0][1][1]],'b')    
     Dic = collections.defaultdict(list)
     i+=1
 
-
+#print(Dic_all)
+#10
 ### Plotting Doors
 ### Plotting using CSV####
 df = pd.read_csv('door_coordinates.csv',sep=',', header=None)
@@ -200,7 +217,7 @@ for i,cor in enumerate(array):
 # placing a door on the other side of the wall
 temp_points = []#points_doors.copy()
 for i,door in enumerate(points_doors):
-    print(facing_doors[i])
+    #print(facing_doors[i])
     if facing_doors[i][0][0] == -1:
         temp_points.append(Point(door.x-0.5,door.y))
         temp_points.append(Point(door.x+0.5,door.y))
@@ -217,9 +234,6 @@ for i,door in enumerate(points_doors):
     
 points_doors = temp_points.copy()
 
-
-
-# Place 2 doors on each side of the wall
 
 # Doing some work on the doors
 # Remove one of the doors that are within certain radius of another door
@@ -245,16 +259,30 @@ points_doors = temp_points.copy()
 
 
 
+#remove 2 outside doors
+points_doors = [ p for p in points_doors if not (123<p.x<128 and 74<p.y<77)]
+
+# remove all doors that are floating
+temp_doors = points_doors.copy()
+for p in temp_doors:
+    if 112<p.x<116 and 79<p.y<81:
+        points_doors.remove(p)
+    if 106<p.x<108 and 81<p.y<83:
+        points_doors.remove(p)
+    if 112<p.x<114 and 91<p.y<94:
+        points_doors.remove(p)
+    if 114<p.x<117 and 93<p.y<94:
+        points_doors.remove(p)
+    if 114<p.x<117 and 94<p.y<95:
+        points_doors.remove(p)
+    if 120<p.x<140 and 99.5<p.y<106:
+        points_doors.remove(p)
+
+#for p in points_doors:
+#    plot_point(p,door=True)
+#plt.show()
 
 
-for p in points_doors:
-    plot_point(p,door=True)
-plt.show()
-
-
-
-
-#Removing unconnected lines
 
 
 
@@ -266,7 +294,11 @@ plt.show()
 #    points_all = points_doors+points_rooms+points_corners
 #
 #else: 
-#points_rooms = [Point(165,56),Point(154,63),Point(168,54),Point(110,56),Point(120,72),Point(110,85),Point(110,104),Point(120,93),Point(110,84),Point(135,103),Point(126,60),Point(118,92),Point(141,57)]
+points_rooms = [Point(165,56),Point(154,63),Point(168,54),Point(110,56),Point(120,72),Point(110,85),Point(110,104),Point(120,93),Point(110,84),Point(135,103),Point(126,60),Point(118,92),Point(141,57)]
+
+points_corners = [Point(170.3,61),Point(170.3,60.14),Point(173.15,61.1),Point(129.3,61),Point(124,61),Point(117.9,61.1),Point(123.9,63.7),Point(123.2,69.2),Point(123.1,75.2),Point(123.1,85.8),Point(123.1,93.6),Point(123.9,95.7),Point(141.3,98.2),Point(137.8,100.8),Point(115.1,98.4),Point(120.5,58.7)]#,Point(124,61),Point(124,61),Point(124,61),Point(124,61),Point(124,61),Point(124,61),Point(124,61),Point(124,61),Point(124,61)]
+
+points_all = points_doors+points_corners+points_rooms
 
 
 
@@ -275,167 +307,196 @@ plt.show()
 
 
 
-
-
-
-##plt.show()
 #    
 ##Plotting the points
-#for p in points_doors:
-#    plot_point(p)
-#for p in points_rooms:
-#    plot_point(p,False)
-#for p in points_corners:
-#    plot_point(p)
-#
-#
-######## Approximate solution to the TSP problem #######
-#
-## Plotting the entire walkable path and making graph of connected nodes 
-## with weighted edges (euclidean distance)
-#G = nx.Graph()
-#for i,p in enumerate(points_all):
-#    # identify the essential nodes in the graph
-#    if p in points_rooms:
-#        G.add_node(i,att=("room", p)) #pos=(p.x,p.y))
-#    else:
-#        G.add_node(i,att=("other",p))
-#    # Checking nodes are traversable to node p
-#    for j,q in enumerate(points_all):
-#        if p==q:
-#            continue
-#        is_walkable = plot_path(p,q,Dic)
-#        if is_walkable:
-#            eucl_dist = round(distance.euclidean([p.x,p.y],[q.x,q.y]),2)
-#            G.add_edge(i,j,weight=eucl_dist)
-#            plt.plot([p.x,q.x],[p.y,q.y],'b')
-#nodes_ordered = sorted(G.nodes())
-#
-## Find shortest path between all "room" nodes using dijkstras algorithm
-#node_rooms = [node for node,at in sorted(G.nodes(data=True)) if at['att'][0]=="room"]
-#dijk_dist = []
-#dijk_pred = []
-##node_rooms.sort()
-#for node in node_rooms:
-#    pred,distance = nx.dijkstra_predecessor_and_distance(G,node,weight='weight')
-#    dijk_dist.append(distance)
-#    dijk_pred.append(pred)
-#
-#
-##Make complete subgraph of all room nodes
-##Make a new graph only including the room nodes called G_rooms
-#G_rooms = nx.Graph()
-#for node,at in G.nodes(data=True):
-#    if at['att'][0]=="room":
-#        G_rooms.add_node(node,att = at['att']) 
-#
-#
-#
-## Compute the distance between each node in G_rooms using the dijk_distance
-#for i,node in enumerate(sorted(G_rooms)):
-#    for node2 in sorted(G_rooms):
-#        if node==node2:
-#            continue
-#        distance= dijk_dist[i][node2]
-#        G_rooms.add_edge(node,node2,weight = distance)
-#
-#
-## Checking if the graph is indeed complete
-##print("Is G graph connected? Returns 1 if graph is complete", nx.density(G))
-##print("Is G_rooms graph connected? Returns 1 if graph is complete", nx.density(G_rooms))
-#
-#
-##Make a minimum spanning tree
-#mst_G_rooms=nx.minimum_spanning_tree(G_rooms)
-#source_node = 18
-#
-## Solve the TSP problem for subgraph using DFS traversal
-#dfs_edges_list = list(nx.dfs_edges(mst_G_rooms,source=source_node))
-#
-#
-#
-##print(list(tsp_tree.nodes()))
-##print(list(nx.dfs_postorder_nodes(G_rooms)))
-#
-##print("The DFS traversal before removing double vertices:")
-##print(dfs_edges_list)
-#
-## Remove double vertices
-#tsp_edges = []
-#for i in range(len(dfs_edges_list)):
-#    if i== 0:
-#        node_pair = dfs_edges_list[i]
-#    elif dfs_edges_list[i-1][1]!=dfs_edges_list[i][0]:
-#        node_pair = tuple([dfs_edges_list[i-1][1],dfs_edges_list[i][1]])
-#    else:
-#       node_pair = dfs_edges_list[i] 
-#    #print(node_pair)
-#    tsp_edges.append(node_pair)
-##Adding the last edge from end to start node in tsp edges       
-#tsp_edges.append(tuple([tsp_edges[-1][1],tsp_edges[0][0]]))
-#
-##print("The DFS traversal after removing double vertices:")
-##print(tsp_edges)
-#
-#
-##### Plotting only the "room" points and their TSP solution
-## Make new figure with the floorplan
-#fig, ax = plt.subplots()
-#for line in Dic.values():
-#    ax.plot([line[0][0][0], line[0][1][0]],[line[0][0][1], line[0][1][1]],'b')    
-#
-## First we plot just the relevant points
-#for p in points_rooms:
-#    plot_point(p,False)
-## Plotting starting node
-#p = G_rooms.nodes(data=True)[source_node]['att'][1]
-#plot_point(p,door=False,starting_node = True)
-#
-## Then we plot the edges
-#for edge in tsp_edges:
-#    p = G_rooms.nodes(data=True)[edge[0]]['att'][1]
-#    q = G_rooms.nodes(data=True)[edge[1]]['att'][1]
-#    plt.plot([p.x,q.x],[p.y,q.y],'b')
-#
-#
-#####Plotting the TSP solution for room and other nodes
-##Make new figure with the floorplan
-#fig, ax = plt.subplots()
-#for line in Dic.values():
-#    ax.plot([line[0][0][0], line[0][1][0]],[line[0][0][1], line[0][1][1]],'b')    
-#
-##First we plot all points
-#for p in points_doors:
-#    plot_point(p)
-#for p in points_rooms:
-#    plot_point(p,False)
-#for p in points_corners:
-#    plot_point(p)
-#
-##Plotting starting node
-#p = G_rooms.nodes(data=True)[source_node]['att'][1]
-#plot_point(p,door=False,starting_node = True)
-#
-#
-##Making a mapping from node values to index values
-#from scipy.interpolate import interp1d
-#m = interp1d([12,22],[0,10])
-#
-#
-## Then we plot the edges of the tsp approximate solution
-#for edge in tsp_edges:
-#    index = int(m(edge[0]))
-#    pred = dijk_pred[index] # These are the predecessor nodes
-#    node_prev = edge[1]
-#    #loop that plots line from end node to all its predecessors until it reaches start node
-#    while node_prev != edge[0]:
-#        node_new = node_prev
-#        node_prev = pred[node_prev][0]
-#        p = G.nodes(data=True)[node_new]['att'][1]
-#        q = G.nodes(data=True)[node_prev]['att'][1]
-#        plt.plot([p.x,q.x],[p.y,q.y],'b')
+for p in points_doors:
+    plot_point(p)
+for p in points_rooms:
+    plot_point(p,False)
+for p in points_corners:
+    plot_point(p)
 #plt.show()
 #
+#
+####### Approximate solution to the TSP problem #######
+
+# Plotting the entire walkable path and making graph of connected nodes 
+# with weighted edges (euclidean distance)
+G = nx.Graph()
+s = 0
+a = 0
+#print(Dic_all)
+for i,p in enumerate(points_all):
+    # This is used only for debugging, such that we dont have to run through all doors 
+    #if i>2:
+    #    break
+    # identify the essential nodes in the graph
+    if p in points_rooms:
+        G.add_node(i,att=("room", p)) #pos=(p.x,p.y))
+    else:
+        G.add_node(i,att=("other",p))
+    # Checking nodes are traversable to node p
+    for j,q in enumerate(points_all):
+        if p==q:
+            continue
+        #print(p)
+        #print(q)
+        #print("-------------------------")
+        #print(Dic_all)
+        is_walkable = plot_path(p,q,Dic_all)
+        if is_walkable:
+            s +=1
+            eucl_dist = round(distance.euclidean([p.x,p.y],[q.x,q.y]),2)
+            G.add_edge(i,j,weight=eucl_dist)
+            plt.plot([p.x,q.x],[p.y,q.y],'b')
+        else:
+            a+=1
+nodes_ordered = sorted(G.nodes())
+print("# walkable paths", s)
+print("# non walkable paths",a)
+#plt.show()
+
+# Find shortest path between all "room" nodes using dijkstras algorithm
+node_rooms = [node for node,at in sorted(G.nodes(data=True)) if at['att'][0]=="room"]
+dijk_dist = []
+dijk_pred = []
+#node_rooms.sort()
+for node in node_rooms:
+    pred,distance = nx.dijkstra_predecessor_and_distance(G,node,weight='weight')
+    dijk_dist.append(distance)
+    dijk_pred.append(pred)
+
+#Make complete subgraph of all room nodes
+#Make a new graph only including the room nodes called G_rooms
+G_rooms = nx.Graph()
+for node,at in G.nodes(data=True):
+    if at['att'][0]=="room":
+        G_rooms.add_node(node,att = at['att']) 
+
+#print(dijk_dist)
+#print("pred: ")
+#print(pred)
+# Compute the distance between each node in G_rooms using the dijk_distance
+for i,node in enumerate(sorted(G_rooms)):
+    for node2 in sorted(G_rooms):
+        if node==node2:
+            continue
+        #print(i)
+        #print(dijk_dist[i][node2])
+        distance= dijk_dist[i][node2]
+        G_rooms.add_edge(node,node2,weight = distance)
+
+
+# Checking if the graph is indeed complete
+#print("Is G graph connected? Returns 1 if graph is complete", nx.density(G))
+#print("Is G_rooms graph connected? Returns 1 if graph is complete", nx.density(G_rooms))
+
+
+#Make a minimum spanning tree
+mst_G_rooms=nx.minimum_spanning_tree(G_rooms)
+ 
+
+
+source_node = random.choice(list(G_rooms.nodes))
+
+
+# Solve the TSP problem for subgraph using DFS traversal
+dfs_edges_list = list(nx.dfs_edges(mst_G_rooms,source=source_node))
+
+
+
+#print(list(tsp_tree.nodes()))
+#print(list(nx.dfs_postorder_nodes(G_rooms)))
+
+#print("The DFS traversal before removing double vertices:")
+#print(dfs_edges_list)
+
+# Remove double vertices
+tsp_edges = []
+for i in range(len(dfs_edges_list)):
+    if i== 0:
+        node_pair = dfs_edges_list[i]
+    elif dfs_edges_list[i-1][1]!=dfs_edges_list[i][0]:
+        node_pair = tuple([dfs_edges_list[i-1][1],dfs_edges_list[i][1]])
+    else:
+       node_pair = dfs_edges_list[i] 
+    #print(node_pair)
+    tsp_edges.append(node_pair)
+#Adding the last edge from end to start node in tsp edges       
+tsp_edges.append(tuple([tsp_edges[-1][1],tsp_edges[0][0]]))
+
+#print("The DFS traversal after removing double vertices:")
+#print(tsp_edges)
+
+
+#### Plotting only the "room" points and their TSP solution
+# Make new figure with the floorplan
+fig, ax = plt.subplots()
+for line in Dic.values():
+    ax.plot([line[0][0][0], line[0][1][0]],[line[0][0][1], line[0][1][1]],'b')    
+
+# First we plot just the relevant points
+for p in points_rooms:
+    plot_point(p,False)
+# Plotting starting node
+p = G_rooms.nodes(data=True)[source_node]['att'][1]
+plot_point(p,door=False,starting_node = True)
+
+# Then we plot the edges
+for edge in tsp_edges:
+    p = G_rooms.nodes(data=True)[edge[0]]['att'][1]
+    q = G_rooms.nodes(data=True)[edge[1]]['att'][1]
+    plt.plot([p.x,q.x],[p.y,q.y],'b')
+
+
+####Plotting the TSP solution for room and other nodes
+#Make new figure with the floorplan
+fig, ax = plt.subplots()
+for line in Dic_all.values():
+    ax.plot([line[0][0][0], line[0][1][0]],[line[0][0][1], line[0][1][1]],'b')    
+
+#First we plot all points
+for p in points_doors:
+    plot_point(p)
+for p in points_rooms:
+    plot_point(p,False)
+for p in points_corners:
+    plot_point(p)
+
+#Plotting starting node
+p = G_rooms.nodes(data=True)[source_node]['att'][1]
+plot_point(p,door=False,starting_node = True)
+
+
+#Making a mapping from node values to index values
+from scipy.interpolate import interp1d
+min_node = min(list(G_rooms.nodes))
+max_node = max(list(G_rooms.nodes))
+range_val = max_node-min_node
+
+print(min_node)
+print(max_node)
+print(range_val)
+
+
+m = interp1d([min_node,max_node],[0,range_val])
+
+
+# Then we plot the edges of the tsp approximate solution
+for edge in tsp_edges:
+    #print(edge[0])
+    index = int(m(edge[0]))
+    pred = dijk_pred[index] # These are the predecessor nodes
+    node_prev = edge[1]
+    #loop that plots line from end node to all its predecessors until it reaches start node
+    while node_prev != edge[0]:
+        node_new = node_prev
+        node_prev = pred[node_prev][0]
+        p = G.nodes(data=True)[node_new]['att'][1]
+        q = G.nodes(data=True)[node_prev]['att'][1]
+        plt.plot([p.x,q.x],[p.y,q.y],'b')
+plt.show()
+
 
 
 
